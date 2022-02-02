f6efbbb2b8b0934d6e7ecae43c4a3a7586f876cc
Catarina Mateus <catarinamateus@Catarinas-MBP.lan>
2022-02-02 12:46:33 +0000

    adding material ui icons

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import TabNavigator from './src';
+import AppContextProvider from './src/context';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <TabNavigator />
-    </NavigationContainer>
+    <AppContextProvider>
+      <NavigationContainer>
+        <TabNavigator />
+      </NavigationContainer>
+    </AppContextProvider>
   );
 }
---
+++ src/components/MaterialIcon/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import {Icon, IconProps} from 'react-native-material-ui';
+
+const MaterialIcon = (props: IconProps): JSX.Element => {
+  return <Icon {...props} />;
+};
+
+export default MaterialIcon;
