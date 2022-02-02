36bf2e8057853ff5e8f551a5d027f70e6e4d551e
Catarina Mateus <catarinamateus@Catarinas-MBP.lan>
2022-02-02 10:29:11 +0000

    adding catalogue screen

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import Home from './screens/Home';
+import Catalogue from './screens/Catalogue';
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
     <Tab.Navigator>
+      <Tab.Screen name="Catalogue" component={Catalogue} />
       <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Settingsß" component={Home} />
     </Tab.Navigator>
   );
 };
